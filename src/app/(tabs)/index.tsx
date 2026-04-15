d015e869279b1b2970ed8f13c986d6861cb64fea
Zack Hoang <zackhoang2004@gmail.com>
2026-04-15 15:09:46 -0700

    Add schedule

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,3 +1,5 @@
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -9,7 +11,24 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Schedule screen</Text>
+      <Text>Welcome to</Text>
+      <Text>Cloud Summit 2026</Text>
+      <Text>Friday, May 1st, 2026</Text>
+      <View style={{ flexDirection: "row", gap: 50 }}>
+        <Link href="/(tabs)/schedule">
+          <View style={{ alignItems: "center" }}>
+            <MaterialIcons size={56} name="home" />
+            <Text>Schedule</Text>
+          </View>
+        </Link>
+
+        <Link href="/(tabs)/map">
+          <View style={{ alignItems: "center" }}>
+            <MaterialIcons size={56} name="map" />
+            <Text>Map</Text>
+          </View>
+        </Link>
+      </View>
     </View>
   );
 }
